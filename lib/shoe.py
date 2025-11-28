8969d380da8bf6ea578f0af8a4a13e96daefac68
Grace Odongo <graceakinyi024@gmail.com>
2025-11-28 15:20:33 +0300

    new

--- lib/shoe.py
+++ lib/shoe.py
@@ -1,4 +1,25 @@
 #!/usr/bin/env python3
 
 class Shoe:
-    pass
+    def __init__(self, brand, size):
+        self.brand = brand
+        self._size = None
+        self.size = size
+
+   
+    @property
+    def size(self):
+        return self._size
+
+    @size.setter
+    def size(self, value):
+        if not isinstance(value, int):
+            print("size must be an integer")
+        elif value < 0:
+            print("size must be greater than or equal to 0")
+        else:
+            self._size = value
+
+    def cobble(self):
+        self.condition = "New"
+        print("Your shoe is as good as new!")
